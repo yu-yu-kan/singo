c0911aaacbb5247d981141dbbec900e001897293
yuyuyu <arrowses19960613@icloud.com>
2019-01-04 00:42:15 +0000

    singo4

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 $(function() {
     $(".my-button").click(function() {
         $(".my-button").prop("disabled", true);
+        
         setTimeout(function() {
             $(".signal-skyblue").css("background-color", "skyblue");
         }, 1000);
+        
         setTimeout(function() {
             $(".signal-skyblue").css("background-color", "grey");
         }, 2000);
+        
         setTimeout(function() {
             $(".signal-yellow").css("background-color",  "yellow");
         }, 2000);
+        
         setTimeout(function() {
             $(".signal-yellow").css("background-color", "grey");
         }, 3000);
+        
         setTimeout(function() {
             $(".signal-red").css("background-color", "red");
         }, 3000);
+        
         setTimeout(function() {
             $(".signal-red").css("background-color", "grey");
         }, 4000);
+        
         setTimeout(function() {
             $(".signal-skyblue").css("background-color", "skyblue");
         }, 4000);
